7fb587558953dba1b9fb33170fbeceaa0de86632
Andrej Jokic <aj.jokic@gmail.com>
2020-01-04 20:56:08 +0100

    Initial commit

---
+++ naucna-centrala-andrej/src/main/java/ftn/uns/ac/rs/naucnacentralaandrej/NaucnaCentralaAndrejApplication.java
@@ -0,0 +1,13 @@
+package ftn.uns.ac.rs.naucnacentralaandrej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaucnaCentralaAndrejApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaucnaCentralaAndrejApplication.class, args);
+	}
+
+}
---
+++ naucna-centrala-andrej/src/test/java/ftn/uns/ac/rs/naucnacentralaandrej/NaucnaCentralaAndrejApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.uns.ac.rs.naucnacentralaandrej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaucnaCentralaAndrejApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
